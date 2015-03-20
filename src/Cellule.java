fe8000380866867aa6314e4c46480920e5d635f9
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-03-20 19:52:04 +0100

    Remplissage de Cellule et Coordonnees

--- src/Cellule.java
+++ src/Cellule.java
@@ -1,42 +1,47 @@
+import java.awt.Robot;
+
 
 public abstract class Cellule {
-
-	protected int mine = 0;
-	protected int base = 0;
-	private Robot robot;
-	private Coordonnees coordonnees;
-	String image;
-	
-	public Cellule(int largeur, int hauteur) {
-		
-	}
-	
-	public int contientMine() {
-		return this.mine;
-	}
-	
-	public int estBase() {
-		return this.base;
-	}
-	
-	public Robot getRobot() {
-		return this.robot;
-	}
-	
-	public Coordonnees getCoordonnees() {
-		return this.coordonnees;
-	}
-	
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
-	
-	abstract void deplaceSur(Robot robot);
-	
-	abstract void ajoute(int equipe);
-	
-	abstract void videCase();
-	
+  
+  protected int mine = 0;
+  protected int base = 0;
+  private Robot robot;
+  private Coordonnees coordonnees;
+  String image;
+  
+  public Cellule(int largeur, int hauteur) {
+    this.coordonnees = new Coordonnees(largeur, hauteur);
+  }
+  
+  public int contientMine(){
+    if (this.coordonnees.equals(robot.getCoordonnees())) {
+      return this.mine;
+    }
+    return 0;
+  }
+  
+  public int estBase(){ 
+    if (this.coordonnees.equals(robot.getCoordonnees())) {
+      return this.base;
+    }
+    return 0;
+  }
+  public Robot getRobot() {
+    return this.robot;
+  }
+  public Coordonnees getCoordonnees() {
+    return this.coordonnees;
+  }
+  
+  @Override
+  public String toString() {
+    return "Cellule [mine=" + this.mine + ", base=" + this.base + ", robot=" + this.robot + ", coordonnees="
+        + this.coordonnees + ", image=" + this.image + "]";
+  }
+  
+  abstract void deplaceSur(Robot robot);
+  
+  abstract void ajoute(int equipe);
+  
+  abstract void videCase();
 }
