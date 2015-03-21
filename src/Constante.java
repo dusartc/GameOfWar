2ec677ada377c2183ebbe5814b53421edecc31cb
Clement Dusart <dusart.clement@gmail.com>
2015-03-21 13:48:19 +0100

    definition de qq constantes
    {energie piegeur/tireur/char, nombre de robots max par equipe, taille
    max du plateau}

--- src/Constante.java
+++ src/Constante.java
@@ -1,6 +1,16 @@
 
 public class Constante {
 
-  private Coordonnees coordonnees;
+  /// robot ///
+  public static final int ENERGIE_PIEGEUR = 30;
+  public static final int ENERGIE_TIREUR  = 20;
+  public static final int ENERGIE_CHAR    = 50;
+  public static final int NB_MAX_ROBOTS_PAR_EQUIPE = 5;
+  
+  /// plateau ///
+  public static final int X_MIN = 5;
+  public static final int X_MAX = 25;
+  public static final int Y_MIN = 10;
+  public static final int Y_MAX = 25;
   
 }
