930fe696327531e3f25bebafe6632910d592665a
dusartc <dusart.clement@gmail.com>
2015-03-23 18:31:04 +0100

    genere un plateau avec un quadrillage

--- src/Plateau.java
+++ src/Plateau.java
@@ -10,24 +10,32 @@ public class Plateau {
 //  private static final int BAS    = 4;
   
   public Plateau(int longueur, int largeur, int pourcentageObstacle) {
+    if (pourcentageObstacle > Constante.OBSTACLES_MAX) {
+      System.err.println("trop d'obstacles ; "+Constante.OBSTACLES_MAX+" sera utiliser");
+      pourcentageObstacle = Constante.OBSTACLES_MAX;
+    }
+    if (pourcentageObstacle < Constante.OBSTACLES_MIN) {
+      System.err.println("pas assez d'obstacles ; "+Constante.OBSTACLES_MIN+" sera utiliser");
+      pourcentageObstacle = Constante.OBSTACLES_MIN;
+    }
     this.plateau = new Cellule[largeur][longueur];
-    for (int i = 0; i < plateau[0].length; i++) {   //
-      for (int j = 0; j < plateau.length; j++) {    // remplissage vide
+    for (int i = 0; i < plateau.length; i++) {   //
+      for (int j = 0; j < plateau[0].length; j++) {    // remplissage vide
         this.plateau[i][j] = new Case(j, i);        //
       }
     }
     this.plateau[0][0] = new Base(0, 0);                                //
-    this.plateau[this.plateau[0].length - 1][this.plateau.length - 1] = // base par default
-        new Base(this.plateau[0].length - 1, this.plateau.length - 1);  //
+    this.plateau[this.plateau.length - 1][this.plateau[0].length - 1] = // base par default
+        new Base(this.plateau.length - 1, this.plateau[0].length - 1);  //
     
-    int nbObstacles = (pourcentageObstacle / 100) * (this.plateau[0].length * this.plateau.length);
+    double nbObstacles = ((double) pourcentageObstacle / 100) * (this.plateau[0].length * this.plateau.length);
     int currentObstacles = 0;
     Random rd = new Random();
     while (currentObstacles < nbObstacles) {
       int rdX = rd.nextInt(this.plateau.length - 2) + 1;
       int rdY = rd.nextInt(this.plateau[0].length - 2) + 1;
-      if (getCellule(rdX, rdY).estVide()) {
-        this.plateau[rdY][rdX] = new Case(rdX, rdY, 1);
+      if (getCellule(rdY, rdX).estVide()) {
+        this.plateau[rdX][rdY] = new Case(rdX, rdY, 1);
         currentObstacles += 1;
       }
     }
@@ -46,6 +54,33 @@ public class Plateau {
     return null;
   }
   
+  @Override
+  public String toString() {
+    String ans= quadrillage() + "\n| B ";
+    for (int i = 0; i < plateau.length; i++) {
+      for (int j = 0; j < plateau[0].length; j++) {
+        if (plateau[i][j] instanceof Case) {
+          Case test = (Case) plateau[i][j];
+          if (test.estPassable()) {
+            ans += "|   ";
+          }else {
+            ans += "| X ";
+          }if (j == plateau[0].length-1) {
+            ans += "|";
+          }
+        }
+      }if(i < plateau.length - 1) {
+        ans += "\n"+quadrillage()+"\n";
+      }
+    }
+    return ans + "| b |\n" + quadrillage();    
+  }
   
-  
+  private String quadrillage(){
+    String ans = "+";
+    for (int i = 0; i < plateau[0].length ; i++) {
+      ans += "---+";
+    }    
+    return ans;
+  }
 }
