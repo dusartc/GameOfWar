70fa65c074d434a4558f85c4e46612aee140f3e0
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-03-31 10:33:07 +0200

    Ajout de la classe Mur pour set des murs

---
+++ src/Mur.java
@@ -0,0 +1,11 @@
+
+public class Mur extends Cellule {
+  
+  public Mur(int largeur, int hauteur){
+    super(largeur, hauteur);
+    this.setMur(true);
+  }
+  public boolean estPassable(){
+    return this.estMur();
+  }
+}
