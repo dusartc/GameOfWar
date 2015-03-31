f8de6519b8dff44eef2bfabc177b693b7594f007
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-03-31 10:29:41 +0200

    Classe pour poser une base a une coordonnees donnée

--- src/Base.java
+++ src/Base.java
@@ -3,27 +3,6 @@ public class Base extends Cellule {
 	
 	public Base(int largeur, int hauteur, int equipe) {
       super(largeur, hauteur);
-      this.ajoute(equipe);
+      this.poseBase(equipe);
 	}
-
-    @Override
-	void deplaceSur(Robot robot) {
-    	
-	}
-
-	@Override
-	void ajoute(int equipe) {
-		this.poseBase(equipe);
-	}
-
-	@Override
-	void videCase() {
-		this.poseBase(0);
-	}
-
-	@Override
-	boolean estVide() {
-	  return this.estBase() > 0;
-	}
-
 }
