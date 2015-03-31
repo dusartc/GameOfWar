dd491f39437cd405f8553029bb729c84bd773333
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-03-31 10:32:32 +0200

    Ajout de la classe Mine

---
+++ src/Mine.java
@@ -0,0 +1,9 @@
+
+public class Mine extends Cellule {
+
+	public Mine(int largeur, int hauteur, int equipe) {
+		super(largeur, hauteur);
+		this.poseMine(equipe);
+		// TODO Auto-generated constructor stub
+	}
+}
