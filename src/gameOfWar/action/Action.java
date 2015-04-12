e8af7d05f9490ded3a21cb2ac345f39657ceee60
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-04-12 18:12:29 +0200

    Ajout de quelque ligne pour la javaDoc

--- src/gameOfWar/action/Action.java
+++ src/gameOfWar/action/Action.java
@@ -7,14 +7,16 @@ import gameOfWar.robot.Robot;
  * @author Alexandre
  */
 public abstract class Action {
+  
   /** La direction que doit emprunter le robot*/
   private Coordonnees direction;
   /** L'objectif que doit atteindre le robot*/
   private Coordonnees objectif;
   /** Le robot */
   private Robot robot;
+  
   /**
-   * Construit un objet Action avec le robot et les coordonnees donnes
+   * Construit un objet Action avec le robot et les coordonnees donnees
    * @param robot le robot qui devra effectuer l'action
    * @param direction la direction de l'action
    */
@@ -22,17 +24,32 @@ public abstract class Action {
     this.robot = robot;
     this.direction = direction;
   }
+  
+  /**
+   * Retourne le robot.
+   * @return
+   */
   public Robot getRobot() {
     return this.robot;
   }
+  
+  /**
+   * Retour les coordonnees de la direction.
+   * @return
+   */
   public Coordonnees getDirection() {
     return this.direction;
   }
+  
+  /**
+   * Retour les coordonnees de l'objectif.
+   * @return
+   */
   public Coordonnees getObjectif() {
     return this.objectif;
   }
   /**
    * agit realise l'action du robot
    */
-  abstract void agit(Cellule caseCible); // Modif du paramètre de type Cellule en caseCible pour moins d'ambigûtée
+  public abstract void agit(Cellule caseCible); // Modif du paramètre de type Cellule en caseCible pour moins d'ambigûtée
 }
