39987ed985b9f4ae5e2800dc176bc0afc58eefba
dusartc <dusart.clement@gmail.com>
2015-04-14 22:56:37 +0200

    mhé

--- src/gameOfWar/config/Mine.java
+++ src/gameOfWar/config/Mine.java
@@ -4,26 +4,10 @@ import gameOfWar.robot.Piegeur;
 
 /**
  * La classe Mine correspond au placement d'une mine.
- * @author Mathieu
+ * @author Clement
  *
  */
 public class Mine {
-
-  /**
-   * Le constructeur construit une Mine par rapport aux coordonnees donnees et a l'equipe
-   * @param largeur
-   * @param hauteur
-   * @param equipe, l'equipe de la Mine.
-   */
-//  public Mine(int largeur, int hauteur, int equipe) {
-//    super(largeur, hauteur);
-//    this.poseMine(equipe);
-//  }
-//  
-//  public Mine(Coordonnees coordonnees, int equipes) {
-//    super(coordonnees.getLargeur(), coordonnees.getHauteur());
-//    this.poseMine(equipes);
-//  }
   
   public Mine(Piegeur piegeur) {
     piegeur.getVue().getPlateau().getCelluleByCoordonnees(piegeur.getObjectif()).poseMine(piegeur.getNumEquipe());
