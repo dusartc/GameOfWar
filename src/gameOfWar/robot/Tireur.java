d92989f5453dacc0a8e82cd0ea4d65faef523edd
dusartc <dusart.clement@gmail.com>
2015-04-15 15:28:44 +0200

    correction

--- src/gameOfWar/robot/Tireur.java
+++ src/gameOfWar/robot/Tireur.java
@@ -1,10 +1,13 @@
 package gameOfWar.robot;
 import gameOfWar.action.Action;
+import gameOfWar.action.Attaque;
+import gameOfWar.action.Deplacement;
 import gameOfWar.config.Constante;
 import gameOfWar.config.Coordonnees;
 import gameOfWar.jeux.Equipe;
 import gameOfWar.jeux.Vue;
 
+import java.util.ArrayList;
 import java.util.List;
 // Touche moi cette belle ArrayList(I).
 
@@ -21,6 +24,51 @@ public class Tireur extends Robot {
   public Tireur(Vue vue, Equipe equipe) {
     super(vue, equipe);
     this.setEnergie(Constante.ENERGIE_TIREUR);
+  }
+
+  @Override
+  public Action choisitAction() {
+    List<Coordonnees> dep = initDep();
+    List<Coordonnees> cible = initCible();
+    if (!cible.isEmpty()) {
+      System.out.println("Vous pouvez :\n\t1 - deplacer\n\t2 - Attaquer");
+      int i = Constante.secureInput(1, 2);
+      switch (i) {
+        case 1:
+          int j = 0;
+          System.out.println("Vous pouvez aller en :");
+          for (Coordonnees c : dep) {
+            System.out.println(j + " : " + c);
+            j += 1;
+          }this.setObjectif(dep.get(Constante.secureInput(0, j-1)));
+          return new Deplacement(this);
+        case 2:
+          int h = 0;
+          System.out.println("Vous pouvez attaquer :");
+          for (Coordonnees c : cible) {
+            System.out.println(h + " : " + c);
+          }this.setObjectif(cible.get(Constante.secureInput(0, h-1)));
+          return new Attaque(this);
+        default:
+          System.out.println("mauvais input @choisitAction()");
+          break;
+      }
+    }else {
+      System.out.println("Aucune cible disponible\nVous ne pouvez que vous deplacer");
+      int j = 0;
+      System.out.println("Vous pouvez aller en :");
+      for (Coordonnees c : dep) {
+        System.out.println(j + " : " + c);
+        j += 1;
+      }this.setObjectif(dep.get(Constante.secureInput(0, j-1)));
+      return new Deplacement(this);
+    }
+    return null;
+  }
+
+  @Override
+  public void estSoigne() {
+    this.setEnergie(Math.min(Constante.ENERGIE_TIREUR, getEnergie() + Constante.SOIN));
   }
 
   @Override
@@ -50,12 +98,7 @@ public class Tireur extends Robot {
 
   @Override
   public String getType() {
-    return getClass().getName();
-  }
-
-  @Override
-  public boolean peutTirer() {
-    return true;
+    return this.getClass().getName().substring(getClass().getName().lastIndexOf(".")+1);
   }
 
   @Override
@@ -77,20 +120,54 @@ public class Tireur extends Robot {
       System.err.println("Impossible de subir un Tir");
     }
   }
-
+  
   @Override
   public String toString() {
     return this.getType() + ", " + super.toString();
   }
-
-  @Override
-  public Action choisitAction() {
-    // TODO Auto-generated method stub
-    return null;
+  
+  private List<Coordonnees> initCible() {
+    List<Coordonnees> cible2 = new ArrayList<Coordonnees>();
+    List<Coordonnees> cible = new ArrayList<Coordonnees>();
+    for (Coordonnees coordonnees : Constante.L_PORTEE_TIREUR) {
+      cible.add(this.getCoordonnees().ajout(coordonnees));
+    }
+    for (int i = 0; i < cible.size(); i++) {
+      Coordonnees c = cible.get(i);
+      try {
+        if (this.getVue().getPlateau().getCelluleByCoordonnees(c).estMur()) {
+          i = (((i+Constante.PORTEE_TIREUR)/Constante.PORTEE_TIREUR)*Constante.PORTEE_TIREUR)-1;
+          continue;
+        }if (this.getVue().getPlateau().getCelluleByCoordonnees(c).estRobot() != this.getEquipe().getEquipe()
+            && this.getVue().getPlateau().getCelluleByCoordonnees(c).estRobot() != 0) {
+          cible2.add(c);
+        }
+      } catch (Exception e) {
+        //System.err.print("@initCible ");
+      }      
+    }
+    return cible2;
   }
   
-  @Override
-  public void estSoigne() {
-    this.setEnergie(Math.min(Constante.ENERGIE_TIREUR, getEnergie() + Constante.SOIN));
+  private List<Coordonnees> initDep() {
+    List<Coordonnees> dep = new ArrayList<Coordonnees>();
+    List<Coordonnees> caillou = new ArrayList<Coordonnees>();
+    for (Coordonnees coordonnees : Constante.DEP_TIREUR) {
+      dep.add(this.getCoordonnees().ajout(coordonnees));
+    }caillou.addAll(dep);
+    for (Coordonnees c : caillou) {
+      if (c.getHauteur() < 0 || c.getLargeur() < 0
+          || c.getHauteur() >= this.getVue().getPlateau().getLongueur()
+          || c.getLargeur() >= this.getVue().getPlateau().getLargeur()) {
+        dep.remove(c);
+      }try {
+        if (this.getVue().getPlateau().getCelluleByCoordonnees(c).estMur()) {
+          dep.remove(c);
+        }
+      } catch (Exception e) {
+        //System.err.println("null pointer");
+      }
+    }
+    return dep;
   }
 }
