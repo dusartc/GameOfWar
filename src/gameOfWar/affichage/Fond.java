3fbcb049c6c138eefb3b24c50ac673b0f317c073
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-05-11 21:24:57 +0200

    La déco c'est bien, surtout avec l'IHM

--- src/gameOfWar/affichage/Fond.java
+++ src/gameOfWar/affichage/Fond.java
@@ -8,12 +8,13 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
-// import gameOfWar.jeux.Images.*;
+//import gameOfWar.jeux.Images.*;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class Fond extends JPanel {
+public class Fond extends JPanel{
   /**
    * 
    */
@@ -21,31 +22,28 @@ public class Fond extends JPanel {
 
   @Override
   protected void paintComponent(Graphics g) {
-    // TODO Auto-generated method stub
-    super.paintComponent(g);
+    //super.paintComponent(g);
     try {
-      Image img =
-          ImageIO.read(new File(
-              "C:\\Users\\Mathieu\\workspace\\chaba\\bin\\gameOfWar\\jeux\\Images\\fond.jpg"));
-      g.drawImage(img, 0, 0, this);
+    	File fond = new File("src/gameOfWar/jeux/Images/fond.jpg");
+    	Image img = ImageIO.read(fond);
+    	g.drawImage(img, 0, 0, this);
     } catch (IOException exception) {
       exception.printStackTrace();
     }
-    Graphics2D graphics2d = (Graphics2D) g;
-    GradientPaint gradientPaint3 =
-        new GradientPaint(0, 0, new Color(0xFF, 0xFF, 0xFF), 0, 90, new Color(110, 110, 110), true);
+    Graphics2D graphics2d = (Graphics2D)g;
+    GradientPaint gradientPaint3 = new GradientPaint(0, 0, new Color(0xFF, 0xFF, 0xFF), 0, 90, new Color(110, 110, 110), true);
     graphics2d.setPaint(gradientPaint3);
     graphics2d.fillRoundRect(-(1), 0, 1025, 75, 20, 20);
     g.setColor(Color.BLACK);
     g.drawRoundRect(-(1), 0, 1025, 75, 20, 20);
-    Font Titre = new Font("Déjà Vu", Font.ITALIC, 52);
+    Font Titre = new Font("Deja Vu", Font.ROMAN_BASELINE, 40);
     g.setFont(Titre);
-    g.setColor(Color.white);
-    g.drawString("Game of war.", (this.getWidth() / 3) + 20, this.getHeight() / 12);
-
-    Font copyright = new Font("Déjà Vu", Font.BOLD, 16);
+    g.setColor(new Color(80,80,80));
+    g.drawString("Game of war.", (this.getWidth()/3)+20, this.getHeight()/12);
+    
+    Font copyright = new Font("D�j� Vu", Font.BOLD, 16);
     g.setFont(copyright);
-    g.setColor(Color.black);
-    g.drawString("@ Copyright.", 12, 680);
+    g.setColor(Color.white);
+    g.drawString("(c) Copyright.", 12, 680);
   }
 }
--- src/gameOfWar/affichage/Menu.java
+++ src/gameOfWar/affichage/Menu.java
@@ -31,62 +31,74 @@ public class Menu extends JFrame implements ActionListener{
   protected JLabel jLabel;
   protected Bouton jButton;
   protected JFrame content;
+  protected JPanel jPanel;
   public JTextField jTextField;
+  
+  public void ecrireJFrame(String nom){
+	  	this.setTitle(nom);
+	  	this.setPreferredSize(new Dimension(1024,720));
+		this.pack();
+		this.setResizable(false);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setContentPane(new Fond());
+		
+		jPanel = new JPanel(new GridBagLayout());
+		jPanel.setPreferredSize(new Dimension(1024,600));
+		jPanel.setLocation(0, 120);
+		//this.setLayout(new GridBagLayout());
+	    GridBagConstraints constraints = new GridBagConstraints();
+	    
+	    getBoutonJeu(constraints, "Jeu", 250,100,90,0,0);
+	    getBoutonJeu(constraints, "Parametre", 250,100,10,0,1);
+	    getBoutonJeu(constraints, "Option", 250,100,0,0,2);
+	    
+	    /*jButton = new Bouton("Parametre");
+	    jButton.setPreferredSize(new Dimension(250,100));
+	    constraints.fill = GridBagConstraints.HORIZONTAL;
+	    constraints.weighty =0.2;
+	    constraints.insets = new Insets(10, 0, 0, 15);
+	    constraints.gridx = 0;
+	    constraints.gridy = 1;
+	    this.getContentPane().add(jButton, constraints);
+	    
+	    jButton = new Bouton("");
+	    
+	    jButton = new Bouton("Option");
+	    jButton.setPreferredSize(new Dimension(250,100));
+	    constraints.fill = GridBagConstraints.HORIZONTAL;
+	    constraints.weighty =0.2;
+	    constraints.gridx = 0;
+	    constraints.gridy = 2;
+	    this.getContentPane().add(jButton, constraints);*/
+	    jButton.addActionListener(this);
+	    this.setVisible(true);
+  }
+
+private void getBoutonJeu(GridBagConstraints constraints, String nom, int x, int y, int insetsTop,int gridx, int gridy) {
+	jButton = new Bouton(nom);
+	jButton.setPreferredSize(new Dimension(x,y));
+	constraints.fill = GridBagConstraints.HORIZONTAL;
+	constraints.weighty = 0.2;
+	if (insetsTop > 0) {
+		constraints.insets = new Insets(insetsTop, 0, 0, 15);
+	}
+	constraints.gridx = gridx;
+	constraints.gridy = gridy;
+	this.getContentPane().add(jButton, constraints);
+}
   
   public Menu(String nom) {
     this.x = 10;
     this.y = 10;
-    this.setPreferredSize(new Dimension(1024,720));
-    this.pack();
-    this.setResizable(false);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    this.setLocationRelativeTo(null);
-    this.setContentPane(new Fond());
-    this.setLayout(new GridBagLayout());
-    this.setVisible(true);
-    
-    GridBagConstraints constraints = new GridBagConstraints();
-    
-    jButton = new Bouton("Jeu");
-    jButton.setPreferredSize(new Dimension(250,100));
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.weighty = 0.2;
-    constraints.insets = new Insets(90, 0, 0, 15);
-    constraints.gridx = 0;
-    constraints.gridy = 0;
-    this.getContentPane().add(jButton, constraints);
-    
-    jButton = new Bouton("Parametre");
-    jButton.setPreferredSize(new Dimension(250,100));
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.weighty =0.2;
-    constraints.insets = new Insets(10, 0, 0, 15);
-    constraints.gridx = 0;
-    constraints.gridy = 1;
-    this.getContentPane().add(jButton, constraints);
-    
-    jButton = new Bouton("Option");
-    jButton.setPreferredSize(new Dimension(250,100));
-    constraints.fill = GridBagConstraints.HORIZONTAL;
-    constraints.weighty =0.2;
-    constraints.gridx = 0;
-    constraints.gridy = 2;
-    this.getContentPane().add(jButton, constraints);
-    jButton.addActionListener(this);
-    jLabel = new JLabel();
+	
+	this.ecrireJFrame(nom);
   }
   
   public void actionPerformed(ActionEvent e) {
-    this.setTitle("OPTIONS");
-    this.setPreferredSize(new Dimension(1024,720));
-    this.pack();
-    this.setResizable(false);
-    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.getContentPane().removeAll();
     this.setContentPane(new Option());
-    this.setLayout(new GridBagLayout());
-    this.setVisible(true);
-    //JButton pd = new JButton("Retour");
-    jTextField = new JTextField(1);
+    jTextField = new JTextField(4);
     jTextField.setText(""+x);
     jTextField.addActionListener(new ActionListener() {
       
@@ -99,24 +111,17 @@ public class Menu extends JFrame implements ActionListener{
         }
       }
     });
-    this.getContentPane().add(jTextField);
-    jTextField = new JTextField(1);
-    jTextField.setText(""+y);
-    jTextField.addActionListener(new ActionListener() {
-      
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        int valueY = Integer.parseInt(jTextField.getText());
-        if (valueY >= Constante.X_MIN && valueY <= Constante.X_MAX ) {
-          jTextField.setText(""+valueY);
-          setX(valueY);
-        }
-      }
-    });
-    jTextField.setLocation(500, 0);
-    jTextField.setPreferredSize(new Dimension(100,50));
-    this.getContentPane().add(jTextField);
-    
+    GridBagConstraints constraints2 = new GridBagConstraints();
+    getBoutonJeu(constraints2, "Retour", 50, 50, 0, 1, 3);
+    jButton.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			ecrireJFrame("Game of War");
+		}
+	});
+    this.revalidate();
+    this.repaint();
+    this.setVisible(true);
   }
 
   public int getX(){
