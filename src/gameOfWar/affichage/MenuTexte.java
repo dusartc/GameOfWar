c72891831940a5fa9bea8b6ac0d51988dda2f069
Mathieu Debaerdemaeker <mathieu.debaerdemaeker@gmail.com>
2015-05-12 01:35:53 +0200

    Ajout des selection de jeux et de selection de robot

--- src/gameOfWar/affichage/MenuTexte.java
+++ src/gameOfWar/affichage/MenuTexte.java
@@ -1,10 +1,19 @@
 package gameOfWar.affichage;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
 import gameOfWar.config.Constante;
 import gameOfWar.config.Coordonnees;
 import gameOfWar.jeux.Equipe;
 import gameOfWar.jeux.IA;
 import gameOfWar.jeux.Plateau;
+import gameOfWar.jeux.Vue;
+import gameOfWar.robot.Char;
+import gameOfWar.robot.Piegeur;
+import gameOfWar.robot.Robot;
+import gameOfWar.robot.Tireur;
 
 /**
  * Affiche des menus en mode texte
@@ -13,6 +22,7 @@ import gameOfWar.jeux.Plateau;
  */
 public class MenuTexte {
 
+	public static Equipe[] ans = new Equipe[2];
   /**
    * Efface l'écran si la console associé prend en charge les codes ANSI
    */
@@ -26,31 +36,41 @@ public class MenuTexte {
    * @param p le plateau qui servira a creer la vue de l'equipe
    * @return Equipe[] les equipes qui s'affronteront sur le plateau
    */
-  public static Equipe[] initialisationEquipes(Plateau p) {
-    Equipe[] ans = new Equipe[2];
+  public static Equipe[] initialisationEquipes(Plateau p) {  
     clearScreen();
     System.out.println(quadrillage("INITIALISATION DES EQUIPES"));
-    ans[0] = new Equipe(choisitPays(), p, Constante.EQUIPE_UN, new Coordonnees(0, 0));
-    // TODO 2eme equipe soit joueur ou IA
-    clearScreen();
-    System.out
-        .println("Voulez-vous affronter l'ordinateur ou un autre joueur ?\n\t1 - Ordinateur\n\t2 - Joueur");
-    int i = Constante.secureInput(1, 2);
-    switch (i) {
-      case 1:
-        System.out.println("Vous allez affronter un ordinateur facile");
-        ans[1] =
-            new IA("IA", p, Constante.EQUIPE_DEUX, new Coordonnees(p.getLargeur() - 1,
+	System.out.println("Qu'elle mode de jeux choisissez-vous ? 1) Joueur vs Joueur, 2) Joueur vs IA, 3) IA VS IA");
+	int choix = Constante.secureInput(1, 3);
+	System.out.println("A combien de robot sera fixer la partie ?");
+	int nbRobot = Constante.secureInput(1, 5);
+	switch (choix) {
+	case 1:
+		System.out.println("Vous allez jouer entre joueur !");
+		ans[0] = new Equipe(choisitPays(), p, Constante.EQUIPE_UN, new Coordonnees(0, 0));
+		choisirRobot(nbRobot, Constante.EQUIPE_UN, false);
+		ans[1] = new Equipe(choisitPays(), p, Constante.EQUIPE_DEUX, new Coordonnees(p.getLargeur() - 1,
                 p.getLongueur() - 1));
-        return ans;
-      case 2:
-        ans[1] =
-            new Equipe(choisitPays(), p, Constante.EQUIPE_DEUX, new Coordonnees(p.getLargeur() - 1,
-                p.getLongueur() - 1));
-        return ans;
-      default:
-        return ans;
-    }
+		choisirRobot(nbRobot, Constante.EQUIPE_DEUX, false);
+		return ans;
+	case 2:
+		System.out.println("Vous allez jouer avec un joueur et un ordinnateur facile !");
+		ans[0] = new Equipe(choisitPays(), p, Constante.EQUIPE_UN, new Coordonnees(0, 0));
+		choisirRobot(nbRobot, Constante.EQUIPE_UN, false);
+		ans[1] = new IA("IA", p, Constante.EQUIPE_DEUX, new Coordonnees(p.getLargeur() - 1,
+	                p.getLongueur() - 1));
+		choisirRobot(nbRobot, Constante.EQUIPE_DEUX, true);
+		return ans;
+	case 3:
+		System.out.println("Deux Ordinnateur facile vont s'affronter !");
+		ans[0] =new IA("IA", p, Constante.EQUIPE_UN, new Coordonnees(0, 0));
+		choisirRobot(nbRobot, Constante.EQUIPE_UN, true);
+		ans[1] = new IA("IA", p, Constante.EQUIPE_DEUX, new Coordonnees(p.getLargeur() - 1,
+	                p.getLongueur() - 1));
+		choisirRobot(nbRobot, Constante.EQUIPE_DEUX, true);
+		return ans;
+	default:
+		return ans;
+	}
   }
 
   /**
@@ -74,6 +94,36 @@ public class MenuTexte {
     return new Plateau(x, y, nb);
   }
 
+  private static void choisirRobot(int nbRobot,int noEquipe, boolean bot){
+	  Random rd = new Random();
+	String[] listeRobotPourLesfeignants = new String[]{"Tireur","Piegeur","Char","Worker"};
+	//List<Robot> neo = new ArrayList<Robot>();
+	while (nbRobot != 0) {
+		System.out.println("Choisis entre la liste des robots pour constituer ton equipe : ");
+		for (int i = 0; i < listeRobotPourLesfeignants.length; i++) {
+			System.out.println("N°"+(i+1)+" "+listeRobotPourLesfeignants[i]);
+		}
+		
+		int choix = (!bot) ? Constante.secureInput(1, listeRobotPourLesfeignants.length) : (rd.nextInt(listeRobotPourLesfeignants.length)+1);
+		switch (choix) {
+			case 1:
+				ans[noEquipe - 1].addRobot(new Tireur(ans[noEquipe - 1].getVue(), ans[noEquipe - 1]));
+				break;
+			case 2:
+				ans[noEquipe - 1].addRobot(new Piegeur(ans[noEquipe - 1].getVue(), ans[noEquipe - 1]));
+				break;
+			case 3:
+				ans[noEquipe - 1].addRobot(new Char(ans[noEquipe - 1].getVue(), ans[noEquipe - 1]));
+				break;
+			case 4:
+				ans[noEquipe - 1].addRobot(new Worker(ans[noEquipe - 1].getVue(), ans[noEquipe - 1]));
+				break;
+			default:
+				System.out.println("Hahahahahaha");
+			}
+			--nbRobot;
+	}  
+  }
   private static String choisitPays() {
     System.out.println("Veuillez choisir votre pays parmis ceux-ci :");
     for (int i = 0; i < Constante.PAYS.length; i++) {
