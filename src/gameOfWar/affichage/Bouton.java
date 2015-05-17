09d59809f09fba38dc785286dbb6bae1db256c68
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-05-17 22:33:27 +0200

    Commentaire 2.0 LOOOL

--- src/gameOfWar/affichage/Bouton.java
+++ src/gameOfWar/affichage/Bouton.java
@@ -30,16 +30,4 @@ public class Bouton extends JButton{
     this.setLocation(x, y);
     this.name=str;
   }
-  
-  /*@Override
-  protected void paintComponent(Graphics g) {
-    // TODO Auto-generated method stub
-    super.paintComponent(g);
-    Graphics2D graphics2d = (Graphics2D)g;
-    GradientPaint gradientPaint = new GradientPaint(0, -(180), new Color(80, 80, 80), 0, 90, new Color(40, 40, 40), true);
-    graphics2d.setPaint(gradientPaint);
-    graphics2d.fillRoundRect(160, 120, 700, 500, 20, 20);
-    g.setColor(Color.BLACK);
-    g.drawRoundRect(160, 120, 700, 500, 20, 20);
-  }*/
 }
