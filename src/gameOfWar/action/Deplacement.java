47b055fae72007f73ba19721e0802bf7c10b377f
dusartc <dusart.clement@gmail.com>
2015-05-17 11:50:02 +0200

    nettoyage

--- src/gameOfWar/action/Deplacement.java
+++ src/gameOfWar/action/Deplacement.java
@@ -21,23 +21,6 @@ public class Deplacement extends Action {
     super(robot);
   }
 
-//  /**
-//   * On donne les directions en fonction du robot
-//   * 
-//   * @param robot
-//   * @param direction
-//   */
-//  public Deplacement(Robot robot, Coordonnees direction) {
-//    super(robot, direction);
-//  }
-
-
-  // @Override
-  // public void agit() {
-  // agit(this.getRobot().getVue().getPlateau().getCelluleByCoordonnees(this.getRobot().getObjectif()));
-  // }
-
-
   /**
    * @see Action
    */
@@ -45,58 +28,15 @@ public class Deplacement extends Action {
     moveTo(getObjectif());
   }
 
-//  /**
-//   * Test avec une Cellule donné si le deplacement est possible vers cette Cellule et retourne Vrai
-//   * ou Faux.
-//   * 
-//   * @param c
-//   * @return
-//   */
-//  public boolean deplacementPossible(Cellule c) {
-//    if (this.getRobot() instanceof Tireur || this.getRobot() instanceof Piegeur) {
-//      Cellule inter =
-//          new Cellule(this.getRobot().getCoordonnees().getLargeur(), this.getRobot()
-//              .getCoordonnees().getHauteur());
-//      Coordonnees coord = inter.getCoordonnees().calculCoordonnees(c.getCoordonnees());
-//      for (int i = 0; i < Constante.DEP_TIREUR.size(); i++) {
-//        if (coord.equals(Constante.DEP_TIREUR.get(i))) {
-//          if (c.estRobot() == 0 || c.contientMine() >= 0
-//              || c.estBase() == this.getRobot().getEquipe().getEquipe() || !c.estMur()) {
-//            return true;
-//          }
-//        }
-//      }
-//    }
-//    if (this.getRobot() instanceof Char) {
-//      Cellule inter =
-//          new Cellule(this.getRobot().getCoordonnees().getLargeur(), this.getRobot()
-//              .getCoordonnees().getHauteur());
-//      // cree une cellule avec les coordonnes du robot courant
-//      Coordonnees coord = inter.getCoordonnees().calculCoordonnees(c.getCoordonnees());
-//      // renvoit a la fonction dans la classe cellule qui calule la distance entre les coordonnes du
-//      // robot et celles de la cellule passee en parametre
-//      for (int i = 0; i < Constante.DEP_CHAR.size(); i++) {
-//        // parcourt le tableau des deplacements du char
-//        if (coord.equals(Constante.DEP_CHAR.get(i))) {
-//          // si les coordonnees calculees sont egales a une des coordonnees de la constante DEP_CHAR
-//          if (c.estRobot() == 0 || c.contientMine() >= 0
-//              || c.estBase() == this.getRobot().getEquipe().getEquipe() || !c.estMur()) {
-//            // et si la cellule est vide alors le deplacement est possible
-//            return true;
-//          }
-//        }
-//      }
-//    }
-//    return false;
-//  }
-
   private void moveTo(Coordonnees c) {
     this.getRobot().getVue().retirerRobot(this.getRobot().getCoordonnees());
-    this.getRobot().getVue().getPlateau().getCelluleByCoordonnees(this.getRobot().getCoordonnees()).setImage(" ");
+    this.getRobot().getVue().getPlateau().getCelluleByCoordonnees(this.getRobot().getCoordonnees())
+        .setImage(" ");
     this.getRobot().setCoordonnees(c);
     this.getRobot().getVue().poserRobot(getRobot(), c);
     this.getRobot().perdEnergieApresDep();
-    this.getRobot().getVue().getPlateau().getCelluleByCoordonnees(c).setImage(this.getRobot().getText());
+    this.getRobot().getVue().getPlateau().getCelluleByCoordonnees(c)
+        .setImage(this.getRobot().getText());
     if (this.getRobot().getVue().getPlateau().getCelluleByCoordonnees(c).contientMine() > 0) {
       System.out.println("EEEEEH BOOOM !!!");
       this.getRobot().subitMine();
