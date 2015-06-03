fbc8b41efeedfd98c66ec72f8b207a39357e5cad
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-06-03 19:51:22 +0200

    Essaye moi ça !

--- src/gameOfWar/affichage/Menu.java
+++ src/gameOfWar/affichage/Menu.java
@@ -4,8 +4,13 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -17,50 +22,65 @@ public class Menu extends JPanel {
    */
   private static final long serialVersionUID = 1L;
 
-  private JPanel jPanel = new Forme(new GridBagLayout());
-  private JButton toutLesBoutons;
-  private int nombreSousTitre = 0;
+  private JLabel sousTitre;
+  private JButton music;
+  private JButton quitter;
+  private JButton jeu;
+  private JButton parametre;
+  private JButton option;
 
   public Menu() {
-    super(new GridBagLayout());
-    GridBagConstraints constraints = new GridBagConstraints();
-    this.getBoutonJeu(constraints, "Jeu", 250, 100, GridBagConstraints.VERTICAL, 50, 0, 1);
-    this.getBoutonJeu(constraints, "Parametre", 250, 100, GridBagConstraints.VERTICAL, 0, 0, 2);
-    this.getBoutonJeu(constraints, "Option", 250, 100, GridBagConstraints.VERTICAL, 0, 0, 3);
-    this.getBoutonJeu(constraints, "X", 50, 50, GridBagConstraints.EAST, 0, 0, 4);
-    this.add(jPanel);
-  }
-
-  public JPanel panel(JButton jButton, GridBagConstraints constraints) {
-    jPanel.add(jButton, constraints);
-    jPanel.setLocation(0, 0);
-    jPanel.setMinimumSize(new Dimension(500, 500));
-    jPanel.setPreferredSize(new Dimension(1024, 720));
-    jPanel.setMaximumSize(new Dimension(1000, 650));
-    if (nombreSousTitre == 0) {
-      JLabel sousTitre = new JLabel("Menu");
-      sousTitre.setFont(new Font("Deja Vu", Font.ROMAN_BASELINE, 45));
-      sousTitre.setLocation(150, 0);
-      jPanel.add(sousTitre);
-      nombreSousTitre++;
+    this.setLayout(null);
+    this.add(new Forme());
+    if (!false) {
+      initComponent();
     }
-    return jPanel;
   }
-
-
-  private void getBoutonJeu(GridBagConstraints constraints, String nom, int x, int y, int fill,
-      int insetsTop, int gridx, int gridy) {
-    toutLesBoutons = new Bouton(nom);
-    toutLesBoutons.setPreferredSize(new Dimension(x, y));
-    constraints.fill = fill;
-    constraints.weightx = 1;
-    if (insetsTop > 0) {
-      constraints.insets = new Insets(insetsTop, 40, 0, 20);
+  
+  public void initComponent(){
+    sousTitre = new JLabel("Menu");
+    sousTitre.setFont(new Font("Deja Vu", Font.ROMAN_BASELINE, 45));
+    sousTitre.setBounds(150, 0, 150,50);
+    music = new Bouton();
+    music.setBounds(950, 25, 30, 30);
+    try {
+      File fond = new File("ressources/Images/music.gif");
+      Image img = ImageIO.read(fond);
+      music.setIcon(new ImageIcon(img));
+    } catch (IOException exception) {
+      exception.printStackTrace();
     }
-    constraints.gridx = gridx;
-    constraints.gridy = gridy;
-    toutLesBoutons.setActionCommand(nom);
-    toutLesBoutons.addActionListener(new ac(jPanel));
-    this.panel(toutLesBoutons, constraints);
+    music.setPreferredSize(new Dimension(17, 16));
+    music.setActionCommand("Play");
+    music.addActionListener(new ac());
+    quitter = new Bouton();
+    quitter.setBounds(920,620,60,60);
+    try {
+      File fond = new File("ressources/Images/quitter.png");
+      Image img = ImageIO.read(fond);
+      quitter.setIcon(new ImageIcon(img));
+    } catch (IOException exception) {
+      exception.printStackTrace();
+    }
+    quitter.setPreferredSize(new Dimension(48, 48));
+    quitter.setActionCommand("X");
+    quitter.addActionListener(new ac());
+    jeu = new JButton("Jeu");
+    jeu.setBounds(250,100, 250, 100);
+    jeu.setActionCommand("Jeu");
+    jeu.addActionListener(new ac());
+    parametre = new JButton("Parametre");
+    parametre.setBounds(250,350, 250, 100);
+    parametre.setActionCommand("JParametre");
+    parametre.addActionListener(new ac());
+    option = new JButton("Option");
+    option.setBounds(250,600, 250, 100);
+    option.setActionCommand("Option");
+    option.addActionListener(new ac());
+    
+    this.add(sousTitre);
+    this.add(music);
+    this.add(quitter);
+    this.add(jeu);
   }
 }
