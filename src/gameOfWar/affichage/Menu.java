0c356b53d471accb087d812ad1ae21d06874c632
Debaerdm <mathieu.debaerdemaeker@gmail.com>
2015-06-03 20:44:51 +0200

    PD

--- src/gameOfWar/affichage/Menu.java
+++ src/gameOfWar/affichage/Menu.java
@@ -1,11 +1,12 @@
 package gameOfWar.affichage;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Insets;
 import java.io.File;
 import java.io.IOException;
 
@@ -17,9 +18,6 @@ import javax.swing.JPanel;
 
 public class Menu extends JPanel {
 
-  /**
-   * 
-   */
   private static final long serialVersionUID = 1L;
 
   private JLabel sousTitre;
@@ -29,22 +27,25 @@ public class Menu extends JPanel {
   private JButton parametre;
   private JButton option;
   private Fenetre f;
+  
+  public Menu(){
+    this.setLayout(null);
+    this.setPreferredSize(new Dimension(900,600));
+    this.setOpaque(false);
+    this.initComponent();
+  }
 
   public Menu(Fenetre f) {
-    this.setLayout(null);
-    this.add(new Forme());
-    if (!false) {
-      initComponent();
-    }
-    this.f = f;
+    this();
+    this.setF(f);
   }
   
   public void initComponent(){
     sousTitre = new JLabel("Menu");
     sousTitre.setFont(new Font("Deja Vu", Font.ROMAN_BASELINE, 45));
-    sousTitre.setBounds(150, 0, 150,50);
+    sousTitre.setBounds(410, 45, 150,50);
     music = new Bouton();
-    music.setBounds(950, 25, 30, 30);
+    music.setBounds(770, 60, 30, 30);
     try {
       File fond = new File("ressources/Images/music.gif");
       Image img = ImageIO.read(fond);
@@ -56,7 +57,7 @@ public class Menu extends JPanel {
     music.setActionCommand("Play");
     music.addActionListener(new ac());
     quitter = new Bouton();
-    quitter.setBounds(920,620,60,60);
+    quitter.setBounds(750, 500,60,60);
     try {
       File fond = new File("ressources/Images/quitter.png");
       Image img = ImageIO.read(fond);
@@ -68,15 +69,15 @@ public class Menu extends JPanel {
     quitter.setActionCommand("X");
     quitter.addActionListener(new ac());
     jeu = new JButton("Jeu");
-    jeu.setBounds(250,100, 250, 100);
+    jeu.setBounds(350,150, 250, 100);
     jeu.setActionCommand("Jeu");
     jeu.addActionListener(new ac());
     parametre = new JButton("Parametre");
-    parametre.setBounds(250,350, 250, 100);
-    parametre.setActionCommand("JParametre");
+    parametre.setBounds(350,300, 250, 100);
+    parametre.setActionCommand("Parametre");
     parametre.addActionListener(new ac());
     option = new JButton("Option");
-    option.setBounds(250,600, 250, 100);
+    option.setBounds(350,450, 250, 100);
     option.setActionCommand("Option");
     option.addActionListener(new ac());
     
@@ -84,5 +85,27 @@ public class Menu extends JPanel {
     this.add(music);
     this.add(quitter);
     this.add(jeu);
+    this.add(parametre);
+    this.add(option);
+  }
+
+  public Fenetre getF() {
+    return f;
+  }
+
+  public void setF(Fenetre f) {
+    this.f = f;
+  }
+  
+  @Override
+  protected void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    Graphics2D graphics2d = (Graphics2D)g;
+    GradientPaint gradientPaint = new GradientPaint(0, -(180), new Color(80, 80, 80), 0, 90, new Color(40, 40, 40), true);
+    graphics2d.setPaint(gradientPaint);
+    graphics2d.fillRoundRect(100, 30, 740, 550, 20, 20);
+    GradientPaint gradientPaint2 = new GradientPaint(0, 0, new Color(0xFF, 0xFF, 0xFF), 0, 90,  new Color(110, 110, 110), true);
+    graphics2d.setPaint(gradientPaint2);
+    graphics2d.fillRoundRect(100, 30, 740, 80, 20, 20);
   }
 }
