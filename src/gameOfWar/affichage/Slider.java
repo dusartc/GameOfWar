c2f868f986e95735416cd6f4d34e94b449e55e0c
dusartc <dusart.clement@gmail.com>
2015-06-05 11:55:01 +0200

    close enough

--- src/gameOfWar/affichage/Slider.java
+++ src/gameOfWar/affichage/Slider.java
@@ -7,27 +7,38 @@ import javax.swing.JSlider;
 public class Slider extends JSlider {
 
   private static final long serialVersionUID = 1L;
-  
+
   private String nom;
 
   public Slider(int min, int max, int x, int y, int w, int h) {
-    //this.setPreferredSize(new Dimension(w,h));
+    // this.setPreferredSize(new Dimension(w,h));
     this.setMaximum(max);
     this.setMinimum(min);
     this.setBounds(x, y, w, h);
     this.setOpaque(false);
-    this.setValue((max + min)/2);
+    this.setValue((max + min) / 2);
     this.setPaintTicks(true);
     this.setPaintLabels(true);
-    this.setMajorTickSpacing((max + min)/4);
+    if (max+min > 100) {
+      this.setMajorTickSpacing(20);
+      this.setMinorTickSpacing(5);
+    }else if (max+min>=50) {
+      this.setMajorTickSpacing(10);
+      this.setMinorTickSpacing(2);
+      this.setMinimum(5);
+    }
+    
+    else {
+      this.setMajorTickSpacing(2);
+    }
     this.setForeground(Color.white);
   }
-  
+
   public Slider(String nom, int min, int max, int x, int y, int w, int h) {
     this(min, max, x, y, w, h);
     this.nom = nom;
   }
-  
+
   public String getNom() {
     return nom;
   }
